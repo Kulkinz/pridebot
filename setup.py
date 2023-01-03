e10fe3f1ed504252fe82308319ab86ab55c14706
Gwen Kornak <cool@coolspages.com>
2023-01-03 12:09:02 -0800

    Setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='pridebot2.0',
+        version='0.1.0',
+        packages=[''],
+        url='',
+        license='',
+        author='gwen',
+        author_email='gwen@gwenkornak.ca',
+        description='Bot to help manage queer discords'
+)
